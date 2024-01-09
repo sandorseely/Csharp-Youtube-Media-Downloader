4b2d374db92ddaf3044fd5b712e486de4fbf8b1d
sandorseely <seelysandor@gmail.com>
2024-01-08 20:32:38 -0500

    Initial commit

---
+++ YoutubeDownloader/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace YoutubeDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.listDownload = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.listStatus = new System.Windows.Forms.ListBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxExtension = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(872, 553);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.comboBoxExtension);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.btnDownload);
+            this.tabPage1.Controls.Add(this.listStatus);
+            this.tabPage1.Controls.Add(this.btnClear);
+            this.tabPage1.Controls.Add(this.listDownload);
+            this.tabPage1.Controls.Add(this.btnAdd);
+            this.tabPage1.Controls.Add(this.txtLink);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(864, 527);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Main";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.linkLabel1);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(864, 527);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "About";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Direct Youtube Link:";
+            // 
+            // txtLink
+            // 
+            this.txtLink.Location = new System.Drawing.Point(24, 37);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(182, 20);
+            this.txtLink.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(24, 63);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(58, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // listDownload
+            // 
+            this.listDownload.FormattingEnabled = true;
+            this.listDownload.Location = new System.Drawing.Point(24, 126);
+            this.listDownload.Name = "listDownload";
+            this.listDownload.Size = new System.Drawing.Size(305, 186);
+            this.listDownload.TabIndex = 4;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(24, 318);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(58, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // listStatus
+            // 
+            this.listStatus.FormattingEnabled = true;
+            this.listStatus.HorizontalScrollbar = true;
+            this.listStatus.Location = new System.Drawing.Point(390, 37);
+            this.listStatus.Name = "listStatus";
+            this.listStatus.ScrollAlwaysVisible = true;
+            this.listStatus.Size = new System.Drawing.Size(468, 472);
+            this.listStatus.TabIndex = 6;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(88, 318);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(118, 23);
+            this.btnDownload.TabIndex = 7;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(426, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tool put together to help me learn how to create sophisticated programs using Win" +
+    "Forms.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Choose Extension:";
+            // 
+            // comboBoxExtension
+            // 
+            this.comboBoxExtension.FormattingEnabled = true;
+            this.comboBoxExtension.Items.AddRange(new object[] {
+            "mp3",
+            "mp4"});
+            this.comboBoxExtension.Location = new System.Drawing.Point(237, 35);
+            this.comboBoxExtension.Name = "comboBoxExtension";
+            this.comboBoxExtension.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxExtension.TabIndex = 9;
+            this.comboBoxExtension.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(387, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Logs";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(9, 130);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(90, 13);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "View Github Here";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 578);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "SJ Youtube to MP3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ListBox listDownload;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listStatus;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxExtension;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ YoutubeDownloader/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+using System.Diagnostics;
+using NAudio;
+using NAudio.Wave;
+using NAudio.Lame;
+
+namespace YoutubeDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static string downloadsDirectory = Path.Combine(Environment.CurrentDirectory, "downloads");
+        private static string extension;
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            listDownload.Items.Add(txtLink.Text);
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listDownload.Items.Clear();
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            string extension = (string)comboBoxExtension.SelectedItem;
+            listStatus.Items.Add("[DEBUG] Selected extension is: " + extension);
+            foreach (string s in listDownload.Items)
+            {
+                int ampersandIndex = s.IndexOf('&');
+                if (ampersandIndex != -1)
+                {
+                    download(s.Substring(0, ampersandIndex), extension);
+                }
+            }          
+
+        }
+
+        private async void download(string link, string extension)
+        {
+            var youtube = new YoutubeClient();
+            var video = await youtube.Videos.GetAsync(link);
+
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(link);
+
+            // Get the audio-only stream with the highest bitrate
+            var audioStreamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+
+            string outputPath = Path.Combine(downloadsDirectory, $"{video.Title}.{extension}");
+
+            listStatus.Items.Add("Attempting download: " + outputPath);
+
+            if (extension == "mp3" && audioStreamInfo != null)
+            {
+                listStatus.Items.Add("[DEBUG] Downloading Webm.");
+                listStatus.Items.Add( "[DEBUG] Starting conversion to Mp3...");
+
+                // Download the WebM audio stream to a temporary file
+                string tempWebmPath = Path.Combine(downloadsDirectory, $"{video.Title}.temp.webm");
+                await youtube.Videos.Streams.DownloadAsync(audioStreamInfo, tempWebmPath);
+
+                // Convert the WebM to MP3 using NAudio
+                using (var reader = new MediaFoundationReader(tempWebmPath))
+                {
+                    string outputPathMp3 = Path.Combine(downloadsDirectory, $"{video.Title}.mp3");
+                    using (var writer = new LameMP3FileWriter(outputPathMp3, reader.WaveFormat, LAMEPreset.VBR_90))
+                    {
+                        reader.CopyTo(writer);
+                    }
+                }
+
+
+                // Delete the temporary WebM file
+                File.Delete(tempWebmPath);
+
+                listStatus.Items.Add("MP3 conversion successful: " + outputPath);
+            }
+            else if (extension == "mp4")
+            {
+                // Get the muxed video stream with the highest video quality
+                var videoStreamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+
+                listStatus.Items.Add("[DEBUG] Downloading mp4.");
+
+                await youtube.Videos.Streams.DownloadAsync(videoStreamInfo, outputPath);
+            }
+
+            if (File.Exists(outputPath))
+            {
+                listStatus.Items.Add("DOWNLOAD SUCCESS: " + outputPath);
+            }
+            else
+            {
+                listStatus.Items.Add("DOWNLOAD FAIL: " + outputPath);
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(downloadsDirectory))
+            {
+                listStatus.Items.Add("Creating downloads directory");
+                Directory.CreateDirectory(downloadsDirectory);
+
+            }
+            else
+            {
+                listStatus.Items.Add("Downloads directory found");
+            }
+
+
+
+
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/sandorseely/Csharp-Youtube-Media-Downloader");
+
+        }
+    }
+}
